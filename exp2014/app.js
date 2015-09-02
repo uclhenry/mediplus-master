9d94b0bd277d290c097090c870ab3cadaf3e5567
Vino Mano <vino.m@me.com>
2015-09-02 13:25:06 +0100

    initial upload

---
+++ exp2014/app.js
@@ -0,0 +1,515 @@
+var express = require('express');
+var path = require('path');
+var mongoose = require('mongoose');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+//var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+//app.use('/app',require('./sb-admin').app).listen(9000);
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+//app.use('/users', users);
+
+// 
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost/test';
+
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+    
+  });
+}
+  
+  app.get('/tophashtags/:shortUrl', function(req, res) {
+
+    var shorturl = ".*" + req.params.shortUrl;
+
+    MongoClient.connect(url, function(err,db) {
+            assert.equal(null, err);
+            var callback = function() {
+              db.close();
+            };
+
+
+              db.collection('mediboard1').aggregate(
+
+                [
+
+              {$match: { "entities.urls.0": { $exists: true }, $and: [ { "entities.hashtags.0": { $exists: true } }, { $and: [ { "entities.urls.0.url": new RegExp(shorturl, 'i') } ] } ] }},{$project: {"entities.hashtags": 1, "entities.urls": 1}  },{$limit: 10}
+
+                ]
+
+                ).toArray(function(err, result) {
+                  assert.equal(err, null);
+                  //console.log(result);
+
+                  var hashmap = {};
+                                                                                            console.log(result + "imhger");
+
+                  for (var key in result) {
+                                                                                            console.log(result[key].entities.hashtags + "imhger");
+
+                    if (result.hasOwnProperty(key)) {
+                                                                        console.log(result[key].entities.hashtags + "imhger");
+
+
+                      for(var key1 in result[key].entities.hashtags) {
+
+                                                  console.log(result[key].entities.hashtags[key1]);
+
+                        if (result[key].entities.hashtags.hasOwnProperty(key1)) {
+                          console.log(result[key].entities.hashtags[key1].text);
+                          if (!hashmap[result[key].entities.hashtags[key1].text]){
+                            hashmap[result[key].entities.hashtags[key1].text] = 1;
+                          }else{
+                            hashmap[result[key].entities.hashtags[key1].text]++;
+                          }
+
+
+
+                        }
+                      }
+
+
+                    }
+                  }
+
+                  result = hashmap
+
+
+
+                  res.send(result);
+                  callback(result);
+                });
+                        
+          });
+
+
+
+  })
+
+  app.get('/topusers/:day/:month/:year', function(req, res) {
+
+      var day = req.params.day;
+      var month = req.params.month;
+      var year = req.params.year;
+
+      var regexString = ".*" + month + " " + day + ".*" + year;
+
+      MongoClient.connect(url, function(err,db) {
+            assert.equal(null, err);
+            var callback = function() {
+              db.close();
+            };
+
+              db.collection('mediboard1').aggregate(
+
+                [
+
+                //{$group : { _id : '$user.id', count : {$sum : 1}}},{$sort : { count: -1}}, {$limit:10} 
+                {$match:{"created_at" : new RegExp(regexString, 'i') }},{$group : { _id : '$user.screen_name', count : {$sum : 1}}},{$sort : { count: -1}}, {$limit:10} 
+
+                ]
+
+                ).toArray(function(err, result) {
+                  assert.equal(err, null);
+                  console.log(result);
+                  res.send(result);
+                  callback(result);
+                });
+                        
+          });
+
+});
+
+app.get('/topusers', function(req, res) {
+
+
+      MongoClient.connect(url, function(err,db) {
+            assert.equal(null, err);
+            var callback = function() {
+              db.close();
+            };
+
+              db.collection('mediboard1').aggregate(
+
+                [
+
+                //{$group : { _id : '$user.id', count : {$sum : 1}}},{$sort : { count: -1}}, {$limit:10} 
+                {$group : { _id : '$user.screen_name', count : {$sum : 1}}},{$sort : { count: -1}}, {$limit:10} 
+
+                ]
+
+                ).toArray(function(err, result) {
+                  assert.equal(err, null);
+                  console.log(result);
+                  res.send(result);
+                  callback(result);
+                });
+                        
+          });
+
+});
+
+app.get('/networkgraph', function(req, res) {
+
+      MongoClient.connect(url, function(err,db) {
+            assert.equal(null, err);
+            var callback = function() {
+              db.close();
+            };
+
+              db.collection('mediboard1').aggregate(
+
+                [
+
+                {$project: {"text" : 1, "retweeted_status.text": 1, "user.id_str": 1,"user.screen_name": 1, "retweeted_status.user.id_str": 1, "retweeted_status.user.screen_name": 1} }, {$limit:1000}
+
+                ]
+
+                ).toArray(function(err, result) {
+                  assert.equal(err, null);
+                  //console.log(result);
+
+                  //code to convert to node edge graph
+
+                  var i=0,N = 10,
+                  E = 50,
+                  g = {
+                    nodes: [],
+                    edges: []
+                  };
+
+                  var segmentSizes = {};
+                  var tweetSegmentMap = {};
+                  var segmentId = 0;
+                  //var keys = [];
+
+                  for (var key in result) {
+                    if (result.hasOwnProperty(key)) {
+                      
+                      /*
+                      var tweetUserId = result[key].user.id_str;//key.user.id_str;
+
+
+ //check if node/edge already exists..
+                      
+                      var alreadyExists = false;
+
+                      for(var key1 in g.nodes){
+                        console.log(g.nodes[key1].id + "her");
+                        if (g.nodes.hasOwnProperty(key1) && g.nodes[key1].id == tweetUserId) {
+                          alreadyExists = true;
+                          console.log("already exists now true" + tweetUserId);
+                          break;
+                        }
+                      }
+                      if (!alreadyExists){
+                        g.nodes.push({
+                          id: ""+tweetUserId,
+                          label: ""+tweetUserId,
+                          x: Math.random(),
+                          y: Math.random(),
+                          size: Math.random(),
+                          color: '#666'
+                        });
+                      }
+                      */
+
+
+                      function genColor (seed) {
+                          color = Math.floor((Math.abs(Math.tan(seed) * 16777215)) % 16777215);
+                          color = color.toString(16);
+                          // pad any colors shorter than 6 characters with leading 0s
+                          while(color.length < 6) {
+                              color = '0' + color;
+                          }
+                          
+                          return color;
+                      }
+
+                      //REMOVE NODES/EDGES WITH LOW CONNECTIVITY.... too many nodes to send across network.
+                      //do this by storing connectivity number and updating it when inputting nodes, adding new nodes with edges...
+
+                      try{
+
+                        if (result[key].retweeted_status.user) {
+                        
+                          var tweetText = result[key].text;
+                          var retweetText = result[key].retweeted_status.text;
+
+                          var tweetUserId = result[key].user.id_str;//key.user.id_str;
+                          var tweetUserName = result[key].user.screen_name;;
+
+
+     //check if node/edge already exists..
+                          
+                          var alreadyExists_tweet = false;
+
+                          for(var key1 in g.nodes){
+                            //console.log(g.nodes[key1].id + "her");
+                            if (g.nodes.hasOwnProperty(key1) && g.nodes[key1].id == tweetUserId) {
+                              alreadyExists_tweet = true;
+                              //console.log("already exists now true" + tweetUserId);
+                              break;
+                            }
+                          }
+
+                          var retweetUserId = result[key].retweeted_status.user.id_str;
+                          var retweetUserName = result[key].retweeted_status.user.screen_name;
+
+
+                          var alreadyExists_retweet = false;
+
+                          for(var key1 in g.nodes){
+                                                    //console.log(g.nodes[key1].id + "here");
+
+                            if (g.nodes.hasOwnProperty(key1) && g.nodes[key1].id == retweetUserId) {
+                              alreadyExists_retweet = true;
+                                                        //console.log("already exists now true" + retweetUserId);
+
+                              break;
+                            }
+                          }
+
+                          if (!alreadyExists_tweet && !alreadyExists_retweet) {
+                            segmentId++;
+                            tweetSegmentMap[tweetUserId] = segmentId;
+                            tweetSegmentMap[retweetUserId] = segmentId;
+                            segmentSizes[segmentId] = 2;
+
+                          }else if(!alreadyExists_tweet && alreadyExists_retweet) {
+                            tweetSegmentMap[tweetUserId] = tweetSegmentMap[retweetUserId];
+                            segmentSizes[tweetSegmentMap[tweetUserId]]++;
+
+                          }else if(alreadyExists_tweet && !alreadyExists_retweet) {
+                            tweetSegmentMap[retweetUserId] = tweetSegmentMap[tweetUserId];
+                            segmentSizes[tweetSegmentMap[tweetUserId]]++;
+
+                          }
+
+
+                          if (!alreadyExists_tweet){
+
+                            
+
+                            g.nodes.push({
+                              id: ""+tweetUserId,
+                              label: ""+tweetUserName/*+","+tweetSegmentMap[tweetUserId]*/,
+                              x: Math.random(),
+                              y: Math.random(),
+                              size: Math.random(),
+                              color: "#"+genColor(tweetSegmentMap[tweetUserId]),
+                              segment: tweetSegmentMap[tweetUserId],
+                              text: tweetText
+                            });
+
+
+
+
+                            
+                          }
+
+
+
+                          
+                          if(!alreadyExists_retweet){
+
+                            
+                            g.nodes.push({
+                              id: ""+retweetUserId,
+                              label: ""+retweetUserName/*+","+tweetSegmentMap[retweetUserId]*/,
+                              x: Math.random(),
+                              y: Math.random(),
+                              size: Math.random(),
+                              color: "#"+genColor(tweetSegmentMap[retweetUserId]),
+                              segment: tweetSegmentMap[retweetUserId],
+                              text: retweetText
+                            });
+
+                            
+                          }
+
+
+                            g.edges.push({
+                              id: 'e' + i,
+                              source: ""+tweetUserId,
+                              target: ""+retweetUserId,
+                              size: Math.random(),
+                              color: "#666"
+                            });
+                            i++;
+                          
+
+                          
+                        }
+
+                      }catch(e){
+
+                        
+
+                      }
+                      
+
+     
+
+                      //keys.push(g);
+                      //keys.push(result[key].user.id_str);
+                                          //break;
+
+                    }
+                  }
+
+
+                  res.send(g);
+                  callback(g);
+                });
+                        
+          });
+
+});
+
+/*
+ app.get('/topusers/:day/:month/:year', function(req, res, next) {
+
+  var day = req.params.day;
+  var month = req.params.month;
+  var year = req.params.year;
+
+      //make use of day month year to perform query on top user for that specific day
+      MongoClient.connect(url, function(err,db) {
+            assert.equal(null, err);
+            var callback = function() {
+              db.close();
+            };
+
+              db.collection('mediboard1').aggregate(
+
+                [
+
+                {$match:{"created_at" : /.*Jul 20.*2015/ }},{$group : { _id : '$user.screen_name', count : {$sum : 1}}},{$sort : { count: -1}}, {$limit:10} 
+
+                ]
+
+                ).toArray(function(err, result) {
+                  assert.equal(err, null);
+                  console.log(result);
+                  res.send(result);
+                  callback(result);
+                });
+                        
+          });
+
+      
+
+});
+
+ app.get('/topusers/', function(req, res) {
+
+
+      MongoClient.connect(url, function(err,db) {
+            assert.equal(null, err);
+            var callback = function() {
+              db.close();
+            };
+
+              db.collection('mediboard1').aggregate(
+
+                [
+
+                {$group : { _id : '$user.id', count : {$sum : 1}}},{$sort : { count: -1}}, {$limit:10} 
+
+                ]
+
+                ).toArray(function(err, result) {
+                  assert.equal(err, null);
+                  console.log(result);
+                  res.send(result);
+                  callback(result);
+                });
+                        
+          });
+
+});*/
+/*
+var tweetdata = db.collection("mediboard1");
+
+
+app.get('/topusers', function(req, res) {
+
+  tweetdata.aggregate(
+
+    [
+    {$group : { _id : '$user.id', count : {$sum : 1}}},{$sort : { count: -1}}, {$limit:10} 
+
+
+    ], function(err, result) {
+      if (err) {
+        console.log(err);
+        res.send(error);
+        return;
+      }
+      res.send(result);
+    console.log(result);
+  })
+});
+
+
+*/
+
+
+
+//catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ sb-admin/app/scripts/controllers/chartContoller.js
@@ -0,0 +1,199 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name sbAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sbAdminApp
+ */
+var sbAdminApp = angular.module('sbAdminApp')
+  .controller('ChartCtrl', ['$scope', '$timeout', '$q', function ($scope, $timeout, $q) {
+
+/*
+
+{ "_id" : "VeronicaNadan", "count" : 2735 }
+{ "_id" : "RockNHot1", "count" : 1741 }
+{ "_id" : "HoneyBadger253", "count" : 1622 }
+{ "_id" : "itsmepanda1", "count" : 1232 }
+{ "_id" : "TannersDad", "count" : 960 }
+{ "_id" : "itsbaxter", "count" : 915 }
+{ "_id" : "NOWinAutism", "count" : 840 }
+{ "_id" : "aspiesmom", "count" : 767 }
+{ "_id" : "chavaray", "count" : 681 }
+{ "_id" : "doritmi", "count" : 618 }
+
+*/
+/*var executed = false;
+angular.element(document).ready(function () {
+            if (!executed){
+                var $http = angular.injector(['ng']).get('$http');
+                
+                
+
+
+                var defer = $q.defer();
+
+
+
+                $http.get("../api/topusers/")
+                .success(function (response) {
+                    
+
+                    defer.resolve(response);
+                    //console.log(response.data);
+                    //$scope.rows = response.data;
+                    //alert("past");
+                })
+                .error(function(err, status) {
+                    defer.reject(err);
+                })
+
+                $scope.promise = defer.promise;
+                $scope.promise.then(
+                    function(v){return v},
+                    function(err){return err}
+                    )
+                .then(
+
+                    function(v){$scope.rows = v},
+                    function(err){$scope.rows = err}
+
+                    )
+                executed = true;
+            }
+
+
+    });*/
+    
+    
+
+    $scope.line = {
+	    labels: ['07','08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29'],
+	    series: ['Twitter'],
+	    data: [
+	      [9933,6802,6290,6279,2026,0,0,0,0,197,3780,2975,2772,2985,4228,3252,3331,3712,2148,2225,3324,3952,2479]
+	    ],
+	    onClick: function (points, evt) {
+	      console.log(points, evt);
+          var selectedPoint = points[0]['label'];
+          //add table refresh/update here.....
+
+          //////////////////loadTable(JSON.stringify(points[0]['label']));
+          //$scope.rows = [];
+
+          //$http.get("http://localhost:3000/api/topusers/07/07/2015").success(funtion (response) {
+        //});
+
+
+            //$scope.rows = [];
+            //$scope.rows.push('response');
+
+                var $http = angular.injector(['ng']).get('$http');
+                
+                
+
+
+                var defer = $q.defer();
+
+
+
+                $http.get("../api/topusers/"+ selectedPoint +"/Jul/2015")
+                .success(function (response) {
+                    
+
+                    defer.resolve(response);
+                    //console.log(response.data);
+                    //$scope.rows = response.data;
+                    //alert("past");
+                })
+                .error(function(err, status) {
+                    defer.reject(err);
+                })
+
+                $scope.promise = defer.promise;
+                $scope.promise.then(
+                    function(v){return v},
+                    function(err){return err}
+                    )
+                .then(
+
+                    function(v){$scope.rows = v},
+                    function(err){$scope.rows = err}
+
+                    )
+                //console.log(defer.promise);
+                //$scope.rows = defer.promise.data;
+
+            
+
+          }
+
+          
+
+
+          //$scope.counter = 3;
+
+          //$scope.addRow = function() {
+
+            //////////////////////////////////$scope.rows.push('Testing');
+            //$scope.counter++;
+
+          //}
+
+
+                    //angular.element(document.getElementById('lineTable-v')).append($compile("<table><tr><td>RandomUser</td><td>939</td></tr></table>")(scope));
+
+
+
+
+	    
+    };
+
+
+    $scope.bar = {
+	    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+		series: ['Series A', 'Series B'],
+
+		data: [
+		   [65, 59, 80, 81, 56, 55, 40],
+		   [28, 48, 40, 19, 86, 27, 90]
+		]
+    	
+    };
+
+    $scope.donut = {
+    	labels: ["Download Sales", "In-Store Sales", "Mail-Order Sales"],
+    	data: [300, 500, 100]
+    };
+
+    $scope.radar = {
+    	labels:["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+
+    	data:[
+    	    [65, 59, 90, 81, 56, 55, 40],
+    	    [28, 48, 40, 19, 96, 27, 100]
+    	]
+    };
+
+    $scope.pie = {
+    	labels : ["Download Sales", "In-Store Sales", "Mail-Order Sales"],
+    	data : [300, 500, 100]
+    };
+
+    $scope.polar = {
+    	labels : ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"],
+    	data : [300, 500, 100, 40, 120]
+    };
+
+    $scope.dynamic = {
+    	labels : ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"],
+    	data : [300, 500, 100, 40, 120],
+    	type : 'PolarArea',
+
+    	toggle : function () 
+    	{
+    		this.type = this.type === 'PolarArea' ?
+    	    'Pie' : 'PolarArea';
+		}
+    };
+}]);
